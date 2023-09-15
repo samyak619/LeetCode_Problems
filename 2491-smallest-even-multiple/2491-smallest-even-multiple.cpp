1d8245321794231f0f412fca74f9d5c0c65feedf
Samyak Khadse <102276238+samyak619@users.noreply.github.com>
2023-09-16 00:47:36 +0530

    Time: 2 ms (45.84%), Space: 6.2 MB (13.17%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.cpp
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        return (n % 2 == 0) ? n : (n * 2);
+        // return (n % 2 == 0) ? n : (n * 2);
+
+        if(n%2==0) return n;
+        else return n*2;
     }
 };
