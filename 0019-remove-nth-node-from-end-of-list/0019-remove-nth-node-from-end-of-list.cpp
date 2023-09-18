de134dd777b5b2f5767849e440d52f4f1309204f
Samyak Khadse <102276238+samyak619@users.noreply.github.com>
2023-09-18 23:51:36 +0530

    Time: 0 ms (100%), Space: 11 MB (7.72%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,16 +11,48 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n){
-        if(head->next == nullptr) return nullptr;
-        ListNode* temp = head; 
-        int len = 0;
-        while(temp != nullptr) {temp = temp->next; len++;}
-        if(n == len) return head->next;
-        ListNode* prev = head;
-        int size = len - n;
-        int i = 1;
-        while(i<size){i++; prev = prev->next;}
-        prev->next = prev->next->next;
-        return head;
+        // if(head->next == nullptr) return nullptr;
+        // ListNode* temp = head; 
+        // int len = 0;
+        // while(temp != nullptr) {temp = temp->next; len++;}
+        // if(n == len) return head->next;
+        // ListNode* prev = head;
+        // int size = len - n;
+        // int i = 1;
+        // while(i<size){i++; prev = prev->next;}
+        // prev->next = prev->next->next;
+        // return head;
+        if (!head || n <= 0) return head;
+
+        ListNode* dummy = new ListNode(0); // Create a dummy node to handle edge cases
+        dummy->next = head;
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+
+        // Move the fast pointer n+1 nodes ahead
+        for (int i = 0; i < n + 1; ++i) {
+            if (!fast) {
+                // Handle cases where n is greater than or equal to the length of the list
+                delete dummy;
+                return head;
+            }
+            fast = fast->next;
+        }
+
+        // Move both fast and slow pointers until fast reaches the end
+        while (fast) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        // Remove the nth node from the end
+        ListNode* toRemove = slow->next;
+        slow->next = toRemove->next;
+        delete toRemove;
+
+        ListNode* newHead = dummy->next;
+        delete dummy;
+
+        return newHead;
     }
 };
