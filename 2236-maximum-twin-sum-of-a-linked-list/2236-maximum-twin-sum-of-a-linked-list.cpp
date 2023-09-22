1264c3e03fcd055672a795990799007d40664fa4
Samyak Khadse <102276238+samyak619@users.noreply.github.com>
2023-09-23 02:25:50 +0530

    Time: 167 ms (96.98%), Space: 117.1 MB (60.57%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
@@ -11,80 +11,72 @@
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        // int len = 0;
-        // ListNode * temp = head;
-        // while(temp != nullptr && temp->next != nullptr){
-        //     len++;
-        //     temp = temp->next;
-        // }
-        // ListNode* mid = head;
-        // len = (len/2);
-        // while(len--) mid = mid->next;
-        // ListNode *secNull = mid->next;
-        // ListNode* curr = secNull;
-        // ListNode* prev = nullptr;
-        // ListNode* next = nullptr;
-        // curr->next = prev;
-        // while(curr){
-        //     next = curr->next;
-        //     curr->next = prev;
-        //     prev = curr;
-        //     curr = next;
-        // }
-        // ListNode* head2 = prev;
-        // int sum = 0;
-        // while(head && head2){
-        //     int temp = 0;
-        //     temp = head->val + head2->val;
-        //     sum = max(temp, sum);
-        //     head = head->next;
-        //     head2 = head2->next;
-        // }
-        // return sum;
         int len = 0;
-    ListNode* temp = head;
+        ListNode * temp = head;
+        while(temp != nullptr && temp->next != nullptr){
+            len++;
+            temp = temp->next;
+        }
+        ListNode* mid = head;
+        len = (len/2);
+        while(len--) mid = mid->next;
+        ListNode *secNull = mid->next;
+        ListNode* curr = secNull;
+        ListNode* prev = nullptr;
+        ListNode* next = nullptr;
 
-    // Calculate the length of the linked list
-    while (temp != nullptr && temp->next != nullptr) {
-        len++;
-        temp = temp->next;
-    }
+        while(curr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        ListNode* head2 = prev;
+        int sum = 0;
+        while(head && head2){
+            int temp = 0;
+            temp = head->val + head2->val;
+            sum = max(temp, sum);
+            head = head->next;
+            head2 = head2->next;
+        }
+        return sum;
+    // int len = 0;
+    // ListNode* temp = head;
 
-    // Initialize pointers for finding the middle of the linked list
-    ListNode* mid = head;
-    len = (len / 2);
+    // while (temp != nullptr && temp->next != nullptr) {
+    //     len++;
+    //     temp = temp->next;
+    // }
 
-    // Move the mid pointer to the middle of the linked list
-    while (len--)
-        mid = mid->next;
+    // ListNode* mid = head;
+    // len = (len / 2);
 
-    // Reverse the second half of the linked list
-    ListNode* secNull = mid->next;
-    ListNode* curr = secNull;
-    ListNode* prev = nullptr;
-    ListNode* next = nullptr;
+    // while (len--)
+    //     mid = mid->next;
+    // ListNode* secNull = mid->next;
+    // ListNode* curr = secNull;
+    // ListNode* prev = nullptr;
+    // ListNode* next = nullptr;
     
-    while (curr) {
-        next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-    }
+    // while (curr) {
+    //     next = curr->next;
+    //     curr->next = prev;
+    //     prev = curr;
+    //     curr = next;
+    // }
 
-    ListNode* head2 = prev;
-    int sum = 0;
+    // ListNode* head2 = prev;
+    // int sum = 0;
 
-    // Calculate the maximum sum of corresponding nodes
-    while (head && head2) {
-        int temp = 0;
-        temp = head->val + head2->val;
-        sum = max(temp, sum);
+    // while (head && head2) {
+    //     int temp = 0;
+    //     temp = head->val + head2->val;
+    //     sum = max(temp, sum);
+    //     head = head->next;
+    //     head2 = head2->next;
+    // }
 
-        // Move both pointers to the next nodes
-        head = head->next;
-        head2 = head2->next;
-    }
-
-    return sum;
+    // return sum;
     }
 };
