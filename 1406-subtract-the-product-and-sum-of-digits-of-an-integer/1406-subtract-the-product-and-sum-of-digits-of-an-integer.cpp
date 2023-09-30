082154571c81b44448abe8e7673542770def0404
Samyak Khadse <102276238+samyak619@users.noreply.github.com>
2023-10-01 04:43:43 +0530

    Time: 0 ms (100%), Space: 6.5 MB (8.06%) - LeetHub

--- 1406-subtract-the-product-and-sum-of-digits-of-an-integer/1406-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/1406-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int subtractProductAndSum(int n) {
-        vector<int> a;
         int mul = 1, sum = 0;
-        while(n > 0){
-            a.push_back(n%10);
+        while(n>0){
+            mul = mul*(n%10);
+            sum += (n%10);
             n = n/10;
-        }
-        for(int t: a){
-            mul = mul*t;
-            sum += t;
-            cout<<t<<" ";
         }
         return mul - sum;
     }
