53602a70638aa8206b50b53d33b5dad6b5363c20
Samyak Khadse <102276238+samyak619@users.noreply.github.com>
2023-10-11 19:39:22 +0530

    Time: 0 ms (100%), Space: 7.3 MB (43.01%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,17 +3,22 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>> transpose(n, vector<int>(m, 0));
+        // vector<vector<int>> transpose(n, vector<int>(m, 0));
         for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                transpose[j][i] = matrix[i][j];
+            for(int j = 0; j < i; j++){
+                // transpose[j][i] = matrix[i][j];
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        for(int i = 0; i < m; i++){
-            int temp = 0;
-            for(int j = 0, temp = n - 1; j < n; j++){
-                matrix[i][j] = transpose[i][temp--];
-            }
+        // for(int i = 0; i < n; i++){
+        //     // int temp = 0;
+        //     // for(int j = 0, temp = n - 1; j < n; j++){
+        //     //     matrix[i][j] = transpose[i][temp--];
+        //     // }
+        //     reverse(matrix[i].begin(), matrix[i].end());
+        // }
+        for(int row=0;row<n;row++){
+            reverse(matrix[row].begin(),matrix[row].end());
         }
     }
 };
